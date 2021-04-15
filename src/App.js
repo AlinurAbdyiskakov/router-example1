b707d361a7ff4888834818351df423ecaaaf469c
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-04-15 21:38:44 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import About from"./components/About/About";
+import Home from"./components/Home/Home";
+import Cantacts from"./components/Cantacts/Cantacts";
 
+
+import {Route, Switch} from "react-router";
+import { NavLink } from "react-router-dom";
+import logo from "./imges/logo.svg"
+import './App.css';
 function App() {
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      
+      <div className="Toolbar">
+      <img src={logo} alt="animal"/>
+    
+<ul>
+         <li>
+      
+      <NavLink activeCalassName="active" to="/"exact>Home</NavLink>
+      </li>
+      <li>
+      
+      <NavLink activeCalassName="active" to="/about">about</NavLink>
+      </li>
+      <li>
+      
+      <NavLink activeCalassName="active" to="/cantacts">cantacts</NavLink>
+      </li></ul>
+     
+   </div>
+   <div className="Drower">
+
+ 
+        <ul>
+      <li>
+      
+      <NavLink activeCalassName="active" to="/jaz">jaz</NavLink>
+      </li>
+      <li>
+      
+      <NavLink activeCalassName="active" to="/bass">bass</NavLink>
+      </li>
+      <li>
+      
+      <NavLink activeCalassName="active" to="/rep">rep</NavLink>
+      </li>
+      <li>
+      
+      <NavLink activeCalassName="active" to="/jam">jam</NavLink>
+      </li>
+      <li>
+      
+      <NavLink activeCalassName="active" to="/mashina">mashina</NavLink>
+      </li>
+      <li>
+      
+      <NavLink activeCalassName="active" to="/cantacts1">cantacts1</NavLink>
+      </li>
+      <li>
+      
+      <NavLink activeCalassName="active" to="/down">down</NavLink>
+      </li>
+      </ul>
+   </div>
+   <div className="dom">
+     <Switch>
+     <Route path="/" component={Home} exact/>
+      <Route path="/about" component={About} />
+      <Route path="/Cantacts" component={Cantacts} />
+      
+      <Route path="/" render={() => <h1>heloo woed</h1>}/>
+</Switch>
+
+</div>
     </div>
   );
 }
